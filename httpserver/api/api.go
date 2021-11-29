92e5e75b48cddaf9af76acfde3ad153013eaa721
Joe Zhou <zhehui.zhou@outlook.com>
2021-11-29 00:02:09 -0500

    [CNCAMP A03] Imporve graceful shutdown.

--- httpserver/api/api.go
+++ httpserver/api/api.go
@@ -19,22 +19,22 @@ type Interface interface {
 }
 
 func New() Interface {
-	app := fiber.New(fiber.Config{ReadTimeout: time.Second * 5})
-	app.Use(recover.New())
-	app.Use(cors.New())
-	app.Use(middleware.Header())
-	app.Use(middleware.UserContext())
-	app.Use(middleware.Logger())
-
 	handlers := handler.New()
 
+	fiberApp := fiber.New(fiber.Config{ReadTimeout: time.Second * 5})
+	fiberApp.Use(recover.New())
+	fiberApp.Use(cors.New())
+	fiberApp.Use(middleware.Header())
+	fiberApp.Use(middleware.UserContext())
+	fiberApp.Use(middleware.Logger())
+
 	// The health endpoint.
-	app.All("/healthz", func(c *fiber.Ctx) error {
+	fiberApp.All("/healthz", func(c *fiber.Ctx) error {
 		return c.SendStatus(fiber.StatusOK)
 	})
 
 	// User endpoints.
-	user := app.Group("/user")
+	user := fiberApp.Group("/user")
 	{
 		user.Post("/signup", handlers.User().Signup())
 		user.Post("/login", handlers.User().Login())
@@ -42,27 +42,27 @@ func New() Interface {
 
 	// Crypto endpoints.
 	// Only authenticated user can call, and handlers follow rate-limiting rules.
-	cp := app.Group("/crypto")
+	cp := fiberApp.Group("/crypto")
 	cp.Use(middleware.AuthLimiter())
 	{
 		cp.Get("/:crypto_code", handlers.Crypto().GetByCode())
 	}
 
 	return &apiImpl{
-		app:     app,
-		port:    config.Instance().GetPort(),
-		handler: handlers,
+		fiberApp: fiberApp,
+		port:     config.Instance().GetPort(),
+		handler:  handlers,
 	}
 }
 
 type apiImpl struct {
-	app     *fiber.App
-	port    int
-	handler handler.Handler
+	fiberApp *fiber.App
+	port     int
+	handler  handler.Handler
 }
 
 func (api *apiImpl) Run() {
-	err := api.app.Listen(fmt.Sprintf(":%d", api.port))
+	err := api.fiberApp.Listen(fmt.Sprintf(":%d", api.port))
 	if err != nil {
 		api.Shutdown()
 		log.Fatalln(err)
@@ -74,15 +74,15 @@ func (api *apiImpl) Shutdown() {
 
 	log.Info("waiting for active connections to close...")
 
-	if err := api.app.Shutdown(); err != nil {
+	if err := api.fiberApp.Shutdown(); err != nil {
 		log.Error(err)
 	}
-	log.Info("api.app shut down")
+	log.Info("api fiber app shut down")
 
 	if err := api.handler.Shutdown(); err != nil {
 		log.Error(err)
 	}
-	log.Info("api.handler shut down")
+	log.Info("api handler shut down")
 
 	log.Info("api server gracefully shut down")
 }
